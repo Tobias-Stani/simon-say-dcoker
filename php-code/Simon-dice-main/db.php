35a8777f4ee5f4e8bbc9f62616ef3b088c192d25
tobi stani <stanislavskytobias@gmail.com>
2023-09-12 21:29:00 -0300

    agregado que no se puedan introducir nombres iguales

--- php-code/Simon-dice-main/db.php
+++ php-code/Simon-dice-main/db.php
@@ -15,19 +15,39 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-$victorias = $_POST['victorias'];
-$derrotas = $_POST['derrotas'];
-$rondasJugadas = $_POST['rondasJugadas'];
+// Obtén el nombre de usuario enviado a través de POST
 $username = $_POST['username'];
 
-// Prepara una consulta SQL para insertar los datos en una tabla
-$sql = "INSERT INTO usuarios (nombre, rondas_jugadas, veces_gano, veces_perdio) VALUES ('$username', '$rondasJugadas', '$victorias', '$derrotas')";
+// Verifica si el nombre de usuario ya existe en la base de datos
+$sql = "SELECT COUNT(*) as count FROM usuarios WHERE nombre = '$username'";
+$result = $conn->query($sql);
 
-// Ejecuta la consulta
-if ($conn->query($sql) === TRUE) {
-    echo "Datos insertados correctamente";
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $count = $row['count'];
+    
+    if ($count > 0) {
+        // El nombre de usuario ya existe, muestra un mensaje de error
+        echo "El nombre de usuario ya está en uso. Introduce un nombre de usuario diferente.";
+    } else {
+        // El nombre de usuario no existe, puedes continuar con la inserción
+        // Obtén los valores de los contadores y otros datos
+        $victorias = $_POST['victorias'];
+        $derrotas = $_POST['derrotas'];
+        $rondasJugadas = $_POST['rondasJugadas'];
+
+        // Prepara una consulta SQL para insertar los datos en la tabla
+        $insertSql = "INSERT INTO usuarios (nombre, rondas_jugadas, veces_gano, veces_perdio) VALUES ('$username', '$rondasJugadas', '$victorias', '$derrotas')";
+
+        // Ejecuta la consulta de inserción
+        if ($conn->query($insertSql) === TRUE) {
+            echo "Datos insertados correctamente";
+        } else {
+            echo "Error al insertar datos: " . $conn->error;
+        }
+    }
 } else {
-    echo "Error al insertar datos: " . $conn->error;
+    echo "Error al verificar el nombre de usuario: " . $conn->error;
 }
 
 // Cierra la conexión
