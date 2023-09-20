76ebd423da8451b28862f5053658fe0721339235
Tobias Stanislavsky <94922759+Tobias-Stani@users.noreply.github.com>
2023-09-20 16:53:20 -0300

    Update dbConfig.php

--- php-code/dbConfig.php
+++ php-code/dbConfig.php
@@ -1,8 +1,18 @@
 <?php
 
-$nameServer = 'mysql'; 
+//para docker
+
+$nameServer = 'mysqldb'; // o mysql
 $nameUser = 'root';
-$clave = '12345';
+$clave = '1234';
 $nameDb = 'simon';
+
+//para xampp
+
+// $nameServer = 'localhost'; 
+// $nameUser = 'root';
+// $clave = '';
+// $nameDb = 'simon';
+
 
 ?>
